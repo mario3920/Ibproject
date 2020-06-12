4ffcf6f0c6a2b241686b2d439c85499727db9065
mario3920 <mario3920@outlook.com>
2020-06-12 13:35:38 -0300

    a

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -11,5 +11,8 @@ routes.post('/escoteiros', EscoteirosController.create)
 routes.get('/atividades', AtividadesController.index)
 routes.post('/atividades', AtividadesController.create)
 routes.delete('/atividades/:id', AtividadesController.delete)
+routes.get('/teste', (req, res, next) =>{
+    res.send("Hello")
+})
 
 module.exports = routes;
